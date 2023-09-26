6b0aa26159d730737d5ecfac302bab6009ed503a
DiGureev <dianagureev@gmail.com>
2023-09-26 17:09:57 +0300

    project

--- Parser.py
+++ Parser.py
@@ -1,7 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get('https://www.eventbrite.com/d/israel/tel-aviv/')
-page = BeautifulSoup(res.text, 'html.parser')
+res = requests.get('https://allevents.in/tel-aviv/all')
+soup = BeautifulSoup(res.text, 'html.parser')
 
-print(page)
+container_tag = soup.select('.container .meta h3')
+# soup = BeautifulSoup(container_tag[0], 'html.parser')
+# elem = soup.find_all('li')
+for event in container_tag:
+    print(event)
+
+# print(links)
+
+# for link in soup.find_all('a'):
+#     print(link.get('href'))
+# http://example.com/elsie
+# http://example.com/lacie
+# http://example.com/tillie
