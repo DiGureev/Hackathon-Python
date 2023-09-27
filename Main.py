2b3e4ac7cab65a157d09011322e1db0e9d3d813e
DiGureev <dianagureev@gmail.com>
2023-09-27 14:54:18 +0300

    Hackathon

--- Main.py
+++ Main.py
@@ -1,13 +1,12 @@
-from Parser import Parser, AllEvents 
+from Parser import Parser, AllEvents, SecretTlv
 import DB_manager
 
 allevents = AllEvents('https://allevents.in/tel-viv/all')
-# secrettlv = SecretTlv('https://www.secrettelaviv.com/tickets')
+secrettlv = SecretTlv('https://www.secrettelaviv.com/tickets')
+
 
 if __name__ == "__main__":
-    # data = allevents.get_resource_data()
-    data = allevents.get_resource_data(allevents)
+    data = secrettlv.get_resource_data(secrettlv) + allevents.get_resource_data(allevents)
     DB_manager.update_table(data)
     for i in DB_manager.get_events_week():
         print(f'{i[1]}\n{str(i[2])}\n{i[3]}\n')
-
--- Parser.py
+++ Parser.py
@@ -1,15 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-from selenium import webdriver
-
+import cloudscraper
 
 class Parser:
 
     def __init__(self, url):
         self.url = url
         self.res = requests.get(self.url)
-        self.soup = BeautifulSoup(self.res.text, 'html.parser')
+
+        if self.res.status_code == 200:
+            self.soup = BeautifulSoup(self.res.text, 'html.parser')
+        
+        elif self.res.status_code == 403:
+            self.scraper = cloudscraper.create_scraper()
+            self.res = self.scraper.get(self.url).text
+            self.soup = BeautifulSoup(self.res, 'html.parser')
+    
 
     def get_header(self,header_tag):
         headers = []
@@ -71,30 +78,31 @@ class SecretTlv(Parser):
         super().__init__(url)
     
     def get_h3(self):
-        h3 = self.soup.select('table.events-page tbody')
+        tag_list = self.soup.select('.events-table tbody a')
+        h3 = []
+        
+        for index, x in enumerate(tag_list):
+            if index %2 == 0:
+                h3.append(x)        
         return h3
-    
-    def get_date(self):
-        months = self.soup.select('.container .meta-left .up-month')
-        days = self.soup.select('.container .meta-left .up-day')
-        dates = []
-        my_dict = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12' }
-        for index, day in enumerate(days):
-            one_day = days[index].getText()
-            one_month = months[index].getText()
-            date_string = '' #02/28/2023
-            date_string += f'{my_dict[one_month]}/'
-            date_string += f'{one_day[0:2]}/'
-            date_string += '2023'
-            date_obj = datetime.strptime(date_string, '%m/%d/%Y').date()
-            dates.append(date_obj)
-        return dates
 
     def get_a(self):
-        a = self.soup.select('.container .meta a')
-        return a
+        tag_list = self.soup.select('.events-table tbody a')
 
+        return tag_list
+    
+    def get_date(self):
+        tag_img = self.soup.select('td.event-image')
+        list_data = []
+        for element in tag_img:
+            text = element.find_next('td')
+            list_data.append(text.text.replace('\n', '').replace('\r', ''))
 
-# secrettlv = SecretTlv('https://www.secrettelaviv.com/tickets')
+        new_list = []
+        for element in list_data:
+            new_list.append(element.split(' ')[4])
 
-# print(secrettlv.res.status_code)
+        dates = list(map(lambda x: datetime.strptime(x, '%d/%m/%Y').date(), new_list))
+        
+        return dates
+
